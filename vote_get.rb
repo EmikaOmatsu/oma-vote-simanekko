32c56b8fd632067ea175a2a3f0098c4b16bd502b
尾松恵美佳 <iks_81@omatsuemika-no-MacBook-Pro.local>
2014-11-06 09:15:30 +0900

    1日に1度だけ投票できるようにした(現状indexのページを更新しないとできない)

--- vote_get.rb
+++ vote_get.rb
@@ -6,14 +6,26 @@ require 'date'
 
 class MyApp < Sinatra::Base
 
-  get '/' do
+  configure do
+    enable :sessions
+  end
+
+  get '/index' do
+
+    log = File.open(File.expand_path("../log/#{Time.now.strftime '%Y%m%d'}.log", __FILE__), "a")
+    log.puts 'スクリプト開始'
+
     @date = Date.today
-    @vote_result = '本日は既に投票済みです'
-    #@vote_result = '投票完了'
-    #@vote_result = '不明なエラーです'
+    if session[:last_date] != @date then
+      @vote_result = '投票完了'
+    elsif session[:last_date] == @date then
+      @vote_result = '本日は既に投票済みです'
+    else
+      @vote_result = '不明なエラーです'
+    end
+    session[:last_date] = @date
     slim :index
   end
 end
-
 
 MyApp.run!
